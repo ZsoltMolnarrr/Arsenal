37ea1377803f9c43488b23638e66cd2fe32daed1
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-09-19 20:46:37 +0200

    Enable NeoForge module

--- common/src/main/java/net/arsenal/ArsenalMod.java
+++ common/src/main/java/net/arsenal/ArsenalMod.java
@@ -4,11 +4,6 @@ import net.arsenal.config.RangedConfigFile;
 import net.arsenal.item.ArsenalBows;
 import net.arsenal.item.ArsenalShields;
 import net.arsenal.item.ArsenalWeapons;
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
-import net.arsenal.item.Group;
 import net.arsenal.spell.ArsenalEffects;
 import net.arsenal.spell.ArsenalSounds;
 import net.spell_engine.api.config.ConfigFile;
@@ -48,21 +43,23 @@ public class ArsenalMod {
         rangedConfig.refresh();
         shieldConfig.refresh();
         effectConfig.refresh();
+    }
+
+    public static void registerSounds() {
         ArsenalSounds.register();
+    }
 
-        ArsenalEffects.register(effectConfig.value);
-        effectConfig.save();
-
-        Group.GROUP = FabricItemGroup.builder()
-                .icon(Group.ICON)
-                .displayName(Text.translatable(Group.translationKey))
-                .build();
-        Registry.register(Registries.ITEM_GROUP, Group.KEY, Group.GROUP);
+    public static void registerItems() {
         ArsenalWeapons.register(itemConfig.value.weapons);
         itemConfig.save();
         ArsenalBows.register(rangedConfig.value.ranged_weapons);
         rangedConfig.save();
         ArsenalShields.register(shieldConfig.value.shields);
         shieldConfig.save();
+    }
+
+    public static void registerEffects() {
+        ArsenalEffects.register(effectConfig.value);
+        effectConfig.save();
     }
 }
