d1bf493c2b2d357503fc10eb9b601d497edbbc57
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-11-03 08:07:08 +0100

    Fix imports

--- common/src/main/java/net/arsenal/datagen/ArsenalDataGen.java
+++ common/src/main/java/net/arsenal/datagen/ArsenalDataGen.java
@@ -9,8 +9,6 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
-import net.minecraft.item.Item;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
